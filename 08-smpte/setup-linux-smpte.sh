0e73aab37d1b2bb78426dacb6742e4984800f384
Kevin Leake <kevinleake01@gmail.com>
2015-03-22 12:53:17 +0000

    Update setup-linux-smpte.sh

--- 08-smpte/setup-linux-smpte.sh
+++ 08-smpte/setup-linux-smpte.sh
@@ -9,12 +9,12 @@ ffmpeg -f lavfi -i smptebars=r=25 \
 
 ffmpeg -f lavfi -i smptebars=r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
-  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;02': r=30: box=1: boxcolor=0x000000@1" \
+  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target ntsc-dv -frames:v 1000 01-smptebars-0001.dv
 
-ffmpeg -f lavfi -i smptebars=r=30000/1001 \
+ffmpeg -f lavfi -i smptebars=r=24000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
-  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\:00': r=24: box=1: boxcolor=0x000000@1" \
+  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target film-dv -frames:v 1000 01-smptebars-0002.dv
 
 ffmpeg -f lavfi -i smptehdbars=r=25 \
@@ -24,12 +24,12 @@ ffmpeg -f lavfi -i smptehdbars=r=25 \
 
 ffmpeg -f lavfi -i smptehdbars=r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
-  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;02': r=30: box=1: boxcolor=0x000000@1" \
+  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target ntsc-dv -frames:v 1000 02-smptehdbars-0001.dv
 
-ffmpeg -f lavfi -i smptehdbars=r=30000/1001 \
+ffmpeg -f lavfi -i smptehdbars=r=24000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
-  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\:00': r=24: box=1: boxcolor=0x000000@1" \
+  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target film-dv -frames:v 1000 02-smptehdbars-0002.dv
 
 ffmpeg -f lavfi -i testsrc=r=25 \
@@ -39,12 +39,12 @@ ffmpeg -f lavfi -i testsrc=r=25 \
 
 ffmpeg -f lavfi -i testsrc=r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
-  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;02': r=30: box=1: boxcolor=0x000000@1" \
+  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target ntsc-dv -frames:v 1000 03-testsrc-0001.dv
 
-ffmpeg -f lavfi -i testsrc=r=30000/1001 \
+ffmpeg -f lavfi -i testsrc=r=24000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
-  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\:00': r=24: box=1: boxcolor=0x000000@1" \
+  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target film-dv -frames:v 1000 03-testsrc-0002.dv
 
 ffmpeg -f lavfi -i mandelbrot=r=25 \
@@ -54,11 +54,11 @@ ffmpeg -f lavfi -i mandelbrot=r=25 \
 
 ffmpeg -f lavfi -i mandelbrot=r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
-  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;02': r=30: box=1: boxcolor=0x000000@1" \
+  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target ntsc-dv -frames:v 1000 04-mandelbrot-0001.dv
 
-ffmpeg -f lavfi -i mandelbrot=r=30000/1001 \
+ffmpeg -f lavfi -i mandelbrot=r=24000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
-  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\:00': r=24: box=1: boxcolor=0x000000@1" \
+  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target film-dv -frames:v 1000 04-mandelbrot-0002.dv
 
