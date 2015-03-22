d246da2612659b1129db622cdeb9ae878cc026cf
Kevin Leake <kevinleake01@gmail.com>
2015-03-22 11:47:29 +0000

    AV Toolkit

--- 07-tests/setup-linux-tests.sh
+++ 07-tests/setup-linux-tests.sh
@@ -2,15 +2,20 @@
 
 FONTFILE=/usr/share/fonts/TTF/VeraMono.ttf
 
-ffmpeg -f lavfi -i testsrc=r=25 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=60 \
   -vf "drawtext=fontfile=$FONTFILE: \
-  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\:00': r=25: box=1: boxcolor=0x000000@1" \
-  -target pal-dv -frames:v 1000 01-smpte-0000.dv
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -frames:v 1000 01-hiframerate-0000.mkv
 
-ffmpeg -f lavfi -i testsrc=r=30000/1001 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=90 \
   -vf "drawtext=fontfile=$FONTFILE: \
-  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
-  -target ntsc-dv -frames:v 1000 01-smpte-0001.dv
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -frames:v 1000 01-hiframerate-0001.mkv
+
+ffmpeg -f lavfi -i mandelbrot=s=640x480:r=60 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -frames:v 1000 01-hiframerate-0002.mkv
 
 ffmpeg -f lavfi -i testsrc=s=640x480:r=10 \
   -vf "drawtext=fontfile=$FONTFILE: \
@@ -27,6 +32,11 @@ ffmpeg -f lavfi -i testsrc=s=640x480:r=1 \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -frames:v 1000 02-lowframerate-0002.mkv
 
+ffmpeg -f lavfi -i mandelbrot=s=640x480:r=1 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -frames:v 1000 02-lowframerate-0003.mkv
+
 ffmpeg -f lavfi -i testsrc=s=512x512:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
@@ -41,4 +51,19 @@ ffmpeg -f lavfi -i testsrc=s=373x919:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -s 373x919 -frames:v 1000 03-sizetest-0002.mkv
+
+ffmpeg -f lavfi -i mandelbrot=s=512x512:r=25 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -s 512x512 -frames:v 1000 03-sizetest-0003.mkv
+
+ffmpeg -f lavfi -i mandelbrot=s=945x370:r=25 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -s 945x370 -frames:v 1000 03-sizetest-0004.mkv
+
+ffmpeg -f lavfi -i mandelbrot=s=373x919:r=25 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -s 373x919 -frames:v 1000 03-sizetest-0005.mkv
 
