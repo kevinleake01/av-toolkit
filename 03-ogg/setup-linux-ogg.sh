c64da1f4cded6f4887ede937d0e96138436b9d66
Kevin Leake <kevinleake01@gmail.com>
2015-03-27 21:22:22 +0000

    AV Toolkit

--- 03-ogg/setup-linux-ogg.sh
+++ 03-ogg/setup-linux-ogg.sh
@@ -4,6 +4,7 @@ FONTFILE=/usr/share/fonts/TTF/VeraMono.ttf
 
 mkdir 00-samples
 cd 00-samples
+
 wget http://v2v.cc/~j/theora_testsuite/320x240.ogg
 wget http://v2v.cc/~j/theora_testsuite/320x240.ogv
 wget http://v2v.cc/~j/theora_testsuite/320x240.skeleton+cmml.ogv
@@ -19,7 +20,120 @@ wget http://v2v.cc/~j/theora_testsuite/offset_test.ogv
 wget http://v2v.cc/~j/theora_testsuite/sign_irene_cif-3qi-b.ogg
 wget http://v2v.cc/~j/theora_testsuite/chroma_siting_test.ogv
 wget http://v2v.cc/~j/theora_testsuite/0byteframes.ogv
+
 cd ../
+
+mkdir 01-main
+cd 01-main
+
+ffmpeg -f lavfi -i smptebars=r=25 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x576 -r 25 -frames:v 1000 01-smptebars-0000.ogv
+
+ffmpeg -f lavfi -i smptebars=r=30000/1001 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x480 -r 30000/1001 -frames:v 1000 01-smptebars-0001.ogv
+
+ffmpeg -f lavfi -i smptehdbars=r=25 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x576 -r 25 -frames:v 1000 02-smptehdbars-0000.ogv
+
+ffmpeg -f lavfi -i smptehdbars=r=30000/1001 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x480 -r 30000/1001 -frames:v 1000 02-smptehdbars-0001.ogv
+
+ffmpeg -f lavfi -i testsrc=r=25 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x576 -r 25 -frames:v 1000 03-testsrc-0000.ogv
+
+ffmpeg -f lavfi -i testsrc=r=30000/1001 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x480 -r 30000/1001 -frames:v 1000 03-testsrc-0001.ogv
+
+ffmpeg -f lavfi -i rgbtestsrc=r=25 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x576 -r 25 -frames:v 1000 04-rgbtestsrc-0000.ogv
+
+ffmpeg -f lavfi -i rgbtestsrc=r=30000/1001 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x480 -r 30000/1001 -frames:v 1000 04-rgbtestsrc-0001.ogv
+
+ffmpeg -f lavfi -i mandelbrot=r=25 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x576 -r 25 -frames:v 1000 05-mandelbrot-0000.ogv
+
+ffmpeg -f lavfi -i mandelbrot=r=30000/1001 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x480 -r 30000/1001 -frames:v 1000 05-mandelbrot-0001.ogv
+
+melt -profile dv_pal -group in=0 out=1000 \
+  frei0r.test_pat_B 0=5 1=0 \
+  -consumer avformat:06-philips-pm5544-0000-00.dv
+
+melt -profile dv_ntsc -group in=0 out=1000 \
+  frei0r.test_pat_B 0=5 1=0 \
+  -consumer avformat:06-philips-pm5544-0000-01.dv
+
+ffmpeg -i 06-philips-pm5544-0000-00.dv \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x576 -r 25 06-philips-pm5544-0000.ogv
+
+ffmpeg -i 06-philips-pm5544-0000-01.dv \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x480 -r 30000/1001 06-philips-pm5544-0001.ogv
+
+rm 06-philips-pm5544-0000-00.dv
+
+rm 06-philips-pm5544-0000-01.dv
+
+melt -profile dv_pal -group in=0 out=1000 \
+  frei0r.test_pat_B 0=6 1=0 \
+  -consumer avformat:07-fubk-0000-00.dv
+
+melt -profile dv_ntsc -group in=0 out=1000 \
+  frei0r.test_pat_B 0=6 1=0 \
+  -consumer avformat:07-fubk-0000-01.dv
+
+ffmpeg -i 07-fubk-0000-00.dv \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x576 -r 25 07-fubk-0000.ogv
+
+ffmpeg -i 07-fubk-0000-01.dv \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -s 720x480 -r 30000/1001 07-fubk-0001.ogv
+
+rm 07-fubk-0000-00.dv
+
+rm 07-fubk-0000-01.dv
+
+ffmpeg -f lavfi -i testsrc=r=25 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\:00': r=25: box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -frames:v 1000 08-smpte-0000.ogv
+
+ffmpeg -f lavfi -i testsrc=r=30000/1001 \
+  -vf "drawtext=fontfile=$FONTFILE: \
+  fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
+  -acodec libvorbis -vcodec libtheora -b:v 1000k -frames:v 1000 08-smpte-0001.ogv
+
+cd ../
+
+mkdir 02-from-mpeg
+cd 02-from-mpeg
 
 ffmpeg -f lavfi -i smptebars=r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
@@ -280,4 +394,7 @@ ffmpeg2theora 06-smpte-0005.mpg
 ffmpeg2theora 06-smpte-0006.mpg
 
 rm *.mpg
+
+cd ../
+
 
