918fa05db1b91989f14f0437326e9c182b8453f0
Kevin Leake <kevinleake01@gmail.com>
2016-07-26 16:35:14 +0000

    Update setup-linux-mpeg.sh

--- 02-mpeg/setup-linux-mpeg.sh
+++ 02-mpeg/setup-linux-mpeg.sh
@@ -2,212 +2,212 @@
 
 FONTFILE=/usr/share/fonts/TTF/VeraMono.ttf
 
-ffmpeg -f lavfi -i smptebars=r=25 \
+ffmpeg -f lavfi -i smptebars=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-vcd -frames:v 1000 01-smptebars-0000.mpg
 
-ffmpeg -f lavfi -i smptebars=r=25 \
+ffmpeg -f lavfi -i smptebars=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-svcd -frames:v 1000 01-smptebars-0001.mpg
 
-ffmpeg -f lavfi -i smptebars=r=25 \
+ffmpeg -f lavfi -i smptebars=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-dvd -frames:v 1000 01-smptebars-0002.mpg
 
-ffmpeg -f lavfi -i smptebars=r=30000/1001 \
+ffmpeg -f lavfi -i smptebars=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-vcd -frames:v 1000 01-smptebars-0003.mpg
 
-ffmpeg -f lavfi -i smptebars=r=30000/1001 \
+ffmpeg -f lavfi -i smptebars=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-svcd -frames:v 1000 01-smptebars-0004.mpg
 
-ffmpeg -f lavfi -i smptebars=r=30000/1001 \
+ffmpeg -f lavfi -i smptebars=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-dvd -frames:v 1000 01-smptebars-0005.mpg
 
-ffmpeg -f lavfi -i smptebars=r=24000/1001 \
+ffmpeg -f lavfi -i smptebars=s=640x480:r=24000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target film-dvd -frames:v 1000 01-smptebars-0006.mpg
 
-ffmpeg -f lavfi -i smptehdbars=r=25 \
+ffmpeg -f lavfi -i smptehdbars=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-vcd -frames:v 1000 02-smptehdbars-0000.mpg
 
-ffmpeg -f lavfi -i smptehdbars=r=25 \
+ffmpeg -f lavfi -i smptehdbars=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-svcd -frames:v 1000 02-smptehdbars-0001.mpg
 
-ffmpeg -f lavfi -i smptehdbars=r=25 \
+ffmpeg -f lavfi -i smptehdbars=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-dvd -frames:v 1000 02-smptehdbars-0002.mpg
 
-ffmpeg -f lavfi -i smptehdbars=r=30000/1001 \
+ffmpeg -f lavfi -i smptehdbars=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-vcd -frames:v 1000 02-smptehdbars-0003.mpg
 
-ffmpeg -f lavfi -i smptehdbars=r=30000/1001 \
+ffmpeg -f lavfi -i smptehdbars=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-svcd -frames:v 1000 02-smptehdbars-0004.mpg
 
-ffmpeg -f lavfi -i smptehdbars=r=30000/1001 \
+ffmpeg -f lavfi -i smptehdbars=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-dvd -frames:v 1000 02-smptehdbars-0005.mpg
 
-ffmpeg -f lavfi -i smptehdbars=r=24000/1001 \
+ffmpeg -f lavfi -i smptehdbars=s=640x480:r=24000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target film-dvd -frames:v 1000 02-smptehdbars-0006.mpg
 
-ffmpeg -f lavfi -i testsrc=r=25 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-vcd -frames:v 1000 03-testsrc-0000.mpg
 
-ffmpeg -f lavfi -i testsrc=r=25 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-svcd -frames:v 1000 03-testsrc-0001.mpg
 
-ffmpeg -f lavfi -i testsrc=r=25 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-dvd -frames:v 1000 03-testsrc-0002.mpg
 
-ffmpeg -f lavfi -i testsrc=r=30000/1001 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-vcd -frames:v 1000 03-testsrc-0003.mpg
 
-ffmpeg -f lavfi -i testsrc=r=30000/1001 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-svcd -frames:v 1000 03-testsrc-0004.mpg
 
-ffmpeg -f lavfi -i testsrc=r=30000/1001 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-dvd -frames:v 1000 03-testsrc-0005.mpg
 
-ffmpeg -f lavfi -i testsrc=r=24000/1001 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=24000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target film-dvd -frames:v 1000 03-testsrc-0006.mpg
 
-ffmpeg -f lavfi -i rgbtestsrc=r=25 \
+ffmpeg -f lavfi -i rgbtestsrc=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-vcd -frames:v 1000 04-rgbtestsrc-0000.mpg
 
-ffmpeg -f lavfi -i rgbtestsrc=r=25 \
+ffmpeg -f lavfi -i rgbtestsrc=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-svcd -frames:v 1000 04-rgbtestsrc-0001.mpg
 
-ffmpeg -f lavfi -i rgbtestsrc=r=25 \
+ffmpeg -f lavfi -i rgbtestsrc=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-dvd -frames:v 1000 04-rgbtestsrc-0002.mpg
 
-ffmpeg -f lavfi -i rgbtestsrc=r=30000/1001 \
+ffmpeg -f lavfi -i rgbtestsrc=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-vcd -frames:v 1000 04-rgbtestsrc-0003.mpg
 
-ffmpeg -f lavfi -i rgbtestsrc=r=30000/1001 \
+ffmpeg -f lavfi -i rgbtestsrc=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-svcd -frames:v 1000 04-rgbtestsrc-0004.mpg
 
-ffmpeg -f lavfi -i rgbtestsrc=r=30000/1001 \
+ffmpeg -f lavfi -i rgbtestsrc=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-dvd -frames:v 1000 04-rgbtestsrc-0005.mpg
 
-ffmpeg -f lavfi -i rgbtestsrc=r=24000/1001 \
+ffmpeg -f lavfi -i rgbtestsrc=s=640x480:r=24000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target film-dvd -frames:v 1000 04-rgbtestsrc-0006.mpg
 
-ffmpeg -f lavfi -i mandelbrot=r=25 \
+ffmpeg -f lavfi -i mandelbrot=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-vcd -frames:v 1000 05-mandelbrot-0000.mpg
 
-ffmpeg -f lavfi -i mandelbrot=r=25 \
+ffmpeg -f lavfi -i mandelbrot=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-svcd -frames:v 1000 05-mandelbrot-0001.mpg
 
-ffmpeg -f lavfi -i mandelbrot=r=25 \
+ffmpeg -f lavfi -i mandelbrot=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target pal-dvd -frames:v 1000 05-mandelbrot-0002.mpg
 
-ffmpeg -f lavfi -i mandelbrot=r=30000/1001 \
+ffmpeg -f lavfi -i mandelbrot=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-vcd -frames:v 1000 05-mandelbrot-0003.mpg
 
-ffmpeg -f lavfi -i mandelbrot=r=30000/1001 \
+ffmpeg -f lavfi -i mandelbrot=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-svcd -frames:v 1000 05-mandelbrot-0004.mpg
 
-ffmpeg -f lavfi -i mandelbrot=r=30000/1001 \
+ffmpeg -f lavfi -i mandelbrot=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target ntsc-dvd -frames:v 1000 05-mandelbrot-0005.mpg
 
-ffmpeg -f lavfi -i mandelbrot=r=24000/1001 \
+ffmpeg -f lavfi -i mandelbrot=s=640x480:r=24000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='%{pts\:hms}-----%{n}': box=1: boxcolor=0x000000@1" \
   -target film-dvd -frames:v 1000 05-mandelbrot-0006.mpg
 
-ffmpeg -f lavfi -i testsrc=r=25 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\:00': r=25: box=1: boxcolor=0x000000@1" \
   -target pal-vcd -frames:v 1000 06-smpte-0000.mpg
 
-ffmpeg -f lavfi -i testsrc=r=30000/1001 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target ntsc-vcd -frames:v 1000 06-smpte-0001.mpg
 
-ffmpeg -f lavfi -i testsrc=r=25 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\:00': r=25: box=1: boxcolor=0x000000@1" \
   -target pal-svcd -frames:v 1000 06-smpte-0002.mpg
 
-ffmpeg -f lavfi -i testsrc=r=30000/1001 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target ntsc-svcd -frames:v 1000 06-smpte-0003.mpg
 
-ffmpeg -f lavfi -i testsrc=r=25 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=25 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\:00': r=25: box=1: boxcolor=0x000000@1" \
   -target pal-dvd -frames:v 1000 06-smpte-0004.mpg
 
-ffmpeg -f lavfi -i testsrc=r=30000/1001 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=30000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\;00': r=30: box=1: boxcolor=0x000000@1" \
   -target ntsc-dvd -frames:v 1000 06-smpte-0005.mpg
 
-ffmpeg -f lavfi -i testsrc=r=24000/1001 \
+ffmpeg -f lavfi -i testsrc=s=640x480:r=24000/1001 \
   -vf "drawtext=fontfile=$FONTFILE: \
   fontsize=25: fontcolor=0xFFFFFF: text='': timecode='00\:00\:00\:00': r=24000/1001: box=1: boxcolor=0x000000@1" \
   -target film-dvd -frames:v 1000 06-smpte-0006.mpg
